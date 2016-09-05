6d567faa2d012bf3b7889887001e73cfa3fddb4d
tim <tim@192.168.150.196>
2016-09-05 10:46:41 -0600

    Added comment

---
+++ src/assignment3/Animal.java
@@ -0,0 +1,36 @@
+package assignment3;
+
+/**
+ *
+ * REQUIREMENT 2
+ * 
+ * The animal class must be abstract.
+ * 
+ * REQUIREMENT 3
+ * 
+ * The animal class will contain the following immutable parameters:
+ * 
+ * an integer containing the number of legs
+ * a String containing the speakParameter
+ * 
+ * REQUIREMENT 4
+ * 
+ * The animal class will contain the following methods that cannot be modified by
+ * any class that implements an Animal
+ * 
+ * getLegs which returns the number of legs
+ * speak() which prints to the screen the speak parameter.
+ * whereAmI() which prints the current location of the animal.
+ * 
+ * REQUIREMENT 5
+ * 
+ * The animal class will contain two mutable integer values called xCoordinate and yCoordinate
+ * The animal class will contain an abstract method called walkTo(int x, int y)
+ *
+ * 
+ * 
+ * @author tim
+ */
+public abstract class Animal {
+
+}
---
+++ src/assignment3/Assignment3.java
@@ -0,0 +1,104 @@
+package assignment3;
+
+/**
+ * Instructions for this assignment.  Download this template from GitHub, fill in
+ * the author comment below with your name and implement the requirements below.
+ * 
+ * If code is turned in and cannot be compiled, you will receive 0 points.
+ * If a requirement is not implemented, you will receive 0 points.
+ * Requirements are worth 50 points each.
+ * 
+ * @author myname
+ * 
+ * REQUIREMENT 1:
+ * 
+ * Create an ArrayList that contains a list of all animals defined in this program (see Animal class)
+ * Create a for loop that calls the following methods on the animals:
+ * 
+ * (a) getLegs()
+ * (b) speak()
+ * (c) walkTo(0, 1)
+ * (d) whereAmI()
+ * 
+ * See the Animal class for further details
+ * 
+ * CAT CLASS REQUIREMENTS
+ * 
+ * You will need to create a new class called Cat that extends Animal.  It will need
+ * to implement the method walkTo.  The requirements on WalkTo are:
+ * 
+ * CAT_REQUIREMENT 1:
+ * 
+ * This class has no new parameters - the parameters it uses are the ones defined in Animal
+ * Has an implementation of the method WalkTo.  When walk to is called, it will:
+ * 
+ * Print "Walking"
+ * Print "Lying Down"
+ * Set the coordinates of where it was told to walk to.  It must use the Animal coordinates
+ * 
+ * It must call the protected constructor on the Animal and set:
+ * 
+ * legs to 4 and soundParameter to Meow
+ * 
+ * DOG CLASS REQUIREMENTS
+ * 
+ * You will need to create a new class called Dog that extends Animal.  It will need
+ * to implement the method walkTo.  The requirements on WalkTo are:
+ * 
+ * DOG_REQUIREMENT 1:
+ * 
+ * This class has no new parameters - the parameters it uses are the ones defined in Animal
+ * Has an implementation of the method WalkTo.  When walk to is called, it will:
+ * 
+ * Print "Walking"
+ * Print "Wagging Tail"
+ * Set the coordinates of where it was told to walk to.  It must use the Animal coordinates
+ * 
+ * It must call the protected constructor on the Animal and set:
+ * 
+ * legs to 4 and soundParameter to Woof
+ * 
+ * MOUSE CLASS REQUIREMENTS
+ * 
+ * You will need to create a new class called Mouse that extends Animal.  It will need
+ * to implement the method walkTo.  The requirements on WalkTo are:
+ * 
+ * MOUSE_REQUIREMENT 1:
+ * 
+ * This class has no new parameters - the parameters it uses are the ones defined in Animal
+ * Has an implementation of the method WalkTo.  When walk to is called, it will:
+ * 
+ * Print "Walking"
+ * Print "Sniffing ground"
+ * Set the coordinates of where it was told to walk to.  It must use the Animal coordinates
+ * 
+ * It must call the protected constructor on the Animal and set:
+ * 
+ * legs to 4 and soundParameter to Squeek
+ * 
+ * 
+ * HUMAN CLASS REQUIREMENTS
+ * 
+ * You will need to create a new class called Human that extends Animal.  It will need
+ * to implement the method walkTo.  The requirements on WalkTo are:
+ * 
+ * HUMAN_REQUIREMENT 1:
+ * 
+ * This class has no new parameters - the parameters it uses are the ones defined in Animal
+ * Has an implementation of the method WalkTo.  When walk to is called, it will:
+ * 
+ * Print "Walking"
+ * Print "Looking around"
+ * Set the coordinates of where it was told to walk to.  It must use the Animal coordinates
+ * 
+ * It must call the protected constructor on the Animal and set:
+ * 
+ * legs to 2 and soundParameter to Narly
+ * 
+ */
+public class Assignment3 {
+
+    public static void main(String[] args) {
+    }
+    
+}
